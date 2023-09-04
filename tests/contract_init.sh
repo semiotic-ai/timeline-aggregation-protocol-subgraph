eaa84282d2bfcc1207c191c4a340e79695429f3b
carlos.vdr <carlos.vdr@semiotic.ai>
2023-09-04 12:38:56 -0600

    feat: Added all escrow functions in a file
    Signed-off-by: carlos.vdr <carlos.vdr@semiotic.ai>

--- tests/contract_init.sh
+++ tests/contract_init.sh
@@ -7,15 +7,18 @@ RECEIVER=0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b
 # Obtain curren dir
 current_dir=$(pwd)
 
-echo "Step 1: Run graph node contracts"
+echo "Step 1: Run graph contracts"
 cd $current_dir/../contracts
 yarn
 echo "Running node contract deploy"
 FULL_CMD_LOG="$(yes | yarn deploy-localhost --auto-mine)"
+echo "Obtaining Graph address token"
+GRAPH_TOKEN=$(jq '."1337".GraphToken.address' addresses.json -r)
+echo "GRAPH TOKEN = $GRAPH_TOKEN"
+
 cd $current_dir
 echo "Obtaining Graph Node address token"
-GRAPH_NODE=$(python obtain_address.py "$FULL_CMD_LOG")
-echo "GRAPH NODE = $GRAPH_NODE"
+GRAPH_TOKEN=$(python obtain_address.py "$FULL_CMD_LOG")
 
 command cd $current_dir/../timeline-aggregation-protocol-contracts
 command yarn
@@ -27,7 +30,7 @@ ALLOCATION_TRACKER_AD=$(echo $ALLOCATION_VAR | jq -r '.deployedTo')
 echo "Allocation tracker address: $ALLOCATION_TRACKER_AD"
 
 echo "Step 3: Obtain istaking address"
-ISTAKING_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 test/MockStaking.sol:MockStaking --constructor-args $GRAPH_NODE --json)
+ISTAKING_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 test/MockStaking.sol:MockStaking --constructor-args $GRAPH_TOKEN --json)
 ISTAKING_AD=$(echo $ISTAKING_VAR | jq -r '.deployedTo')
 echo "Istaking address: $ISTAKING_AD"
 
@@ -37,18 +40,22 @@ TAP_VERIFIER_AD=$(echo $TAP_VERIFIER_VAR | jq -r '.deployedTo')
 echo "Tap verifier address: $TAP_VERIFIER_AD"
 
 echo "Step 5: Obtain Escrow address"
-ESCROW_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 src/Escrow.sol:Escrow --constructor-args $GRAPH_NODE $ISTAKING_AD $TAP_VERIFIER_AD $ALLOCATION_TRACKER_AD 10 15 --json)
+ESCROW_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 src/Escrow.sol:Escrow --constructor-args $GRAPH_TOKEN $ISTAKING_AD $TAP_VERIFIER_AD $ALLOCATION_TRACKER_AD 10 15 --json)
 ESCROW_AD=$(echo $ESCROW_VAR | jq -r '.deployedTo')
 echo "Escrow address: $ESCROW_AD"
 
 echo "Approving escrow contract"
-cast send --unlocked --from $GATEWAY $GRAPH_NODE 'approve(address spender, uint256 amount)' $ESCROW_AD '10000'
-cast send --unlocked --from $SIGNER $GRAPH_NODE 'approve(address spender, uint256 amount)' $ESCROW_AD '10000'
-cast send --unlocked --from $SIGNER $GRAPH_NODE 'approve(address spender, uint256 amount)' $ESCROW_AD '10000'
-cast send --unlocked --from $GATEWAY $GRAPH_NODE 'setAssetHolder(address, bool)' $ESCROW_AD true
+cast send --unlocked --from $GATEWAY $GRAPH_TOKEN 'approve(address spender, uint256 amount)' $ESCROW_AD '10000'
+cast send --unlocked --from $SIGNER $GRAPH_TOKEN 'approve(address spender, uint256 amount)' $ESCROW_AD '10000'
+cast send --unlocked --from $GATEWAY $GRAPH_TOKEN 'setAssetHolder(address, bool)' $ESCROW_AD true
 # Making sure other accounts have money
-cast send --unlocked --from $GATEWAY $GRAPH_NODE 'transfer(address to, uint256 amount)' $SIGNER 500
-cast send --unlocked --from $GATEWAY $GRAPH_NODE 'transfer(address to, uint256 amount)' $RECEIVER 500
+cast send --unlocked --from $GATEWAY $GRAPH_TOKEN 'transfer(address to, uint256 amount)' $SIGNER 500
+cast send --unlocked --from $GATEWAY $GRAPH_TOKEN 'transfer(address to, uint256 amount)' $RECEIVER 500
 echo "Run commands to deploy subgraph, make sure to use the Escrow Address $ESCROW_AD in the subgraph.yaml"
 echo "Inside /tests/utils.py change ESCROW_CONTRACT with $ESCROW_AD"
+
+cd $current_dir
+
+echo "Running escrow contract calls"
+python escrow_calls.py "$ESCROW_AD" "$TAP_VERIFIER_AD"
 
