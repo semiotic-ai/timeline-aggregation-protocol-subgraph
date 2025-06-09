95e977188b85877a895c2245d7eb201156a8d869
Joseph Livesey <joseph@semiotic.ai>
2025-06-09 14:46:33 -0400

    fix(ci): Add --broadcast flag to forge create for compatibility

--- tests/contract_init.sh
+++ tests/contract_init.sh
@@ -20,22 +20,22 @@ GRAPH_TOKEN=$(jq '."1337".GraphToken.address' addresses.json -r)
 command cd $current_dir/timeline-aggregation-protocol-contracts
 echo "Graph token address: $GRAPH_TOKEN"
 echo "Step 2: Obtain allocation tracker address"
-ALLOCATION_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 src/AllocationIDTracker.sol:AllocationIDTracker --json)
+ALLOCATION_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 src/AllocationIDTracker.sol:AllocationIDTracker --json --broadcast)
 ALLOCATION_TRACKER_AD=$(echo $ALLOCATION_VAR | jq -r '.deployedTo')
 echo "Allocation tracker address: $ALLOCATION_TRACKER_AD"
 
 echo "Step 3: Obtain istaking address"
-ISTAKING_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 test/MockStaking.sol:MockStaking --constructor-args $GRAPH_TOKEN --json)
+ISTAKING_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 test/MockStaking.sol:MockStaking --constructor-args $GRAPH_TOKEN --json --broadcast)
 ISTAKING_AD=$(echo $ISTAKING_VAR | jq -r '.deployedTo')
 echo "Istaking address: $ISTAKING_AD"
 
 echo "Step 4: Obtain TAPVerifier address"
-TAP_VERIFIER_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 src/TAPVerifier.sol:TAPVerifier --constructor-args 'tapVerifier' '1.0' --json)
+TAP_VERIFIER_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 src/TAPVerifier.sol:TAPVerifier --constructor-args 'tapVerifier' '1.0' --json --broadcast)
 TAP_VERIFIER_AD=$(echo $TAP_VERIFIER_VAR | jq -r '.deployedTo')
 echo "Tap verifier address: $TAP_VERIFIER_AD"
 
 echo "Step 5: Obtain Escrow address"
-ESCROW_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 src/Escrow.sol:Escrow --constructor-args $GRAPH_TOKEN $ISTAKING_AD $TAP_VERIFIER_AD $ALLOCATION_TRACKER_AD 10 15 --json)
+ESCROW_VAR=$(forge create --unlocked --from $GATEWAY --rpc-url localhost:8545 src/Escrow.sol:Escrow --constructor-args $GRAPH_TOKEN $ISTAKING_AD $TAP_VERIFIER_AD $ALLOCATION_TRACKER_AD 10 15 --json --broadcast)
 ESCROW_AD=$(echo $ESCROW_VAR | jq -r '.deployedTo')
 echo "Escrow address: $ESCROW_AD"
 
